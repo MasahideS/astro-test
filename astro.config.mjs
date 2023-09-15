248cdf0cc4a024ea5394e4d6ee855bbc296f9053
MasahideS <sakaguchi@mermon.jp>
2023-09-16 00:40:12 +0900

    add-need

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
-
-import netlify from "@astrojs/netlify/functions";
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()],
-  buildOptions: {
-    site: "https://MasahideS.github.io/",
+	integrations: [preact()],
+	buildOptions: {
+		site: "https://MasahideS.github.io/",
     base: "https://github.com/MasahideS/astro-test.git"
-  },
-  output: "server",
-  adapter: netlify()
+	  },
+  output: 'server',
+  adapter: netlify(),
 });
+
